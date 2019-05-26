4f4efe407c50ee586b5b02bea81e187da8001127
unknown <huyn@iastate.edu>
2019-05-26 12:08:04 -0500

    Another commit

--- google kick start/2019/round c/ForeGone_Solution.py
+++ google kick start/2019/round c/ForeGone_Solution.py
@@ -4,32 +4,17 @@
 """
 import sys
 T = int(input().strip())
-def solve(number):
+def solve(instructions,R,C,SR,SC):
     A=[]
     B=[]
-    for num in number[::-1]:
-        if num=="4":
-            A.append("2")
-            B.append("2")
-        elif num =="5":
-            A.append("3")
-            B.append("2")
-        elif num!="0":
-            val = int(num)
-            A.append(str(val-1))
-            B.append("1")
-        else:
-            A.append("0")
-            B.append("0")
-    A = "".join(A[::-1])
-    B = "".join(B[::-1])
+    
     return A,B
             
         
 
                 
 for i in range(T):
-    sys.stdout.flush()
-    number = input().strip()
-    A,B = solve(number)
+    N, R, C, SR , SC = [int(item) for item in input().split()]
+    instructions = input().split()
+    res = solve(instructions,R,C,SR,SC)
     print ("Case #{}: {} {}".format(i+1,A,B))
