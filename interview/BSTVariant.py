79f2d7830572a24f704ed0536a80f8e7ae0bcc7f
unknown <huyn@iastate.edu>
2019-07-31 16:48:15 -0500

    update

--- interview/BSTVariant.py
+++ interview/BSTVariant.py
@@ -90,3 +90,37 @@ def getMin(nums):
     if start ==stop: 
         return nums[start]
     return min(nums[start],nums[stop])
+#https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
+def climbingLeaderboard(scores, alice):
+    d= {}
+    value = set()
+    i = 1
+    for s in scores:
+        if s not in value:
+            d[i]=s
+            value.add(s)
+            i+=1
+    res = []
+    for score in alice:
+        start,stop = 1,len(d)
+        check = False
+        while start+1<stop:
+            mid = (start+stop)//2
+          
+            if d[mid]==score:
+                res.append(mid)
+                check = True
+                break
+            if d[mid]<score:
+                stop = mid
+            else:
+                start = mid
+        if not check:
+            if d[stop]>score:
+                res.append(stop+1)
+            else:
+                if d[start]>score:
+                    res.append(stop)
+                else:
+                    res.append(start)
+    return res
