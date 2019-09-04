fe48ed2cda7e446dd29d88ec650d2ad8044224eb
huyn <huyn@cvm6h4zv52.cvm.iastate.edu>
2019-09-03 19:01:46 -0500

    add

--- interview/graph.py
+++ interview/graph.py
@@ -4,7 +4,7 @@ Created on Mon Sep  2 21:46:44 2019
 
 @author: huyn
 """
-
+import heapq
 #The Ruler of HackerLand believes that every citizen of the country should have access to a library. 
 #Unfortunately, HackerLand was hit by a tornado that destroyed all of its libraries and obstructed its roads! 
 #As you are the greatest programmer of HackerLand, the ruler wants your help to repair the roads and build 
@@ -20,13 +20,40 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
     if c_lib<=c_road:
         return c_lib*n
     d= {}
+
     for edge in cities:
         start,stop = edge
         if start not in d:
-            d[start]=[]
+            d[start]=set()
         if stop not in d:
-            d[stop]=[]
-        d[start].append(stop)
-        d[stop].append(start)
-    
-    return
+            d[stop]=set()
+        d[start].add(stop)
+        d[stop].add(start)
+    # find connected component
+    cc = []
+    visited = set()
+    while cities:
+        start,stop = cities.pop()
+        newComponent = []
+        if start in visited or stop in visited:
+            continue
+        else:
+            queue = [start]
+            while queue:
+                node = queue.pop()
+                visited.add(node)
+                heapq.heappush(newComponent,(-len(d[node]),node))
+                for neighbor in d[start]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+            cc.append(newComponent)
+    minimum = 0
+    for component in cc:
+        minimum+=c_lib+c_road*(len(component)-1)
+    return minimum
+#    for component in cc:
+        
+#n, c_lib, c_road, cities=3,2,1,[[1, 2], [3, 1], [2, 3]]
+#roadsAndLibraries(n, c_lib, c_road, cities)
+#n, c_lib, c_road, cities=6 ,2, 5, [[1, 3], [3, 4], [2, 4], [1, 2], [2, 3], [5, 6]]
+#roadsAndLibraries(n, c_lib, c_road, cities)
