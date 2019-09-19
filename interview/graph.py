ddf14a27342d96e0cd2f11db343fb979c69dd798
nguyenngochuy91 <huyn@cvm6h4zv52.cvm.iastate.edu>
2019-09-19 17:08:13 -0500

    update

--- interview/graph.py
+++ interview/graph.py
@@ -410,3 +410,24 @@ def hasCycleColor(matrix):
 def getAllCycle(matrix):
     n  = len(matrix)
     
+#785. Is Graph Bipartite?
+def isBipartite(graph):
+    d = {}
+    for node,neighbors in enumerate(graph):
+        if node not in d: # have not assign this node, that means any of the edge was not assigned neither
+            stack = [node]
+            d[node] = 0 
+            while stack:
+                node = stack.pop()
+                for neighbor in graph[node]:
+                    if neighbor not in d:
+                        stack.append(neighbor)
+                        d[neighbor]= 1-d[node]
+                    elif d[neighbor]==d[node]:
+                        return False
+    return True
+#graph = [[1,3], [0,2], [1,3], [0,2]]
+#print (isBipartite(graph))
+#graph = [[1,2,3], [0,2], [0,1,3], [0,2]]
+#print (isBipartite(graph))
+        
