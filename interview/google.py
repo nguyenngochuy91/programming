5bb86a1cdee542dd4349112bb6d45747b7b80dc7
Huy <huyn@iastate.edu>
2019-09-22 15:31:12 -0500

    update from leetcode contest

--- interview/google.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 31 18:51:26 2019
-
-@author: huyn
-"""
-
--- interview/hash.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  4 12:57:21 2019
-
-@author: huyn
-"""
-
